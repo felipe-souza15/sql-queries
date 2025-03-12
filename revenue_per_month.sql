cb5aac32fd8b16a0e6af5f88da86b5726d08bb81
Felipe De Souza Lopes <80334159+felipe-souza15@users.noreply.github.com>
2025-03-12 16:37:29 -0300

    Delete revenue_per_month.sql

--- revenue_per_month.sql
+++
@@ -1,8 +0,0 @@
--- Calculates the total revenue per month
-SELECT 
-    YEAR(SaleDate) AS Year, 
-    MONTH(SaleDate) AS Month, 
-    SUM(TotalValue) AS TotalRevenue
-FROM Sales
-GROUP BY YEAR(SaleDate), MONTH(SaleDate)
-ORDER BY Year DESC, Month DESC;
